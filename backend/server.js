956d97fd4245ab31345dd1b734017e2cb3d2ed9a
Hamza Ashfaq <hamzaashfaq4322@gmail.com>
2025-07-25 05:57:35 +0500

    v1

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.get('/health', (req, res) => {
+    res.json({ status: 'OK', message: 'Server is healthy' });
 });
 
 // Routes
