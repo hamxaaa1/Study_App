d8af9cf36892f16c0c41b97f7b6cb1e025c1f186
Hamza Ashfaq <hamzaashfaq4322@gmail.com>
2025-07-25 05:54:58 +0500

    v1

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/notes', require('./routes/noteRoutes'));
